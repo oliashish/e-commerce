9df40b582d066cb731b1cb666eadde4ff56090bf
Ashish oli <oliashish3@gmail.com>
2021-04-30 12:00:22 +0530

    sequelize config done with a Users table in mysql db, db successfully connected

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const db = require("./models");
 
 const route = require("./routes/routes");
 
@@ -11,12 +12,19 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/test", route);
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../../client/build"));
-}
-app.use("/*", (req, res) => {
-    res.send(path.join(__dirname, "..", "..", "client", "build", "index.html"));
-});
+// app.use("/test", route);
+// if (process.env.NODE_ENV === "production") {
+//     app.use(express.static("../../client/build"));
+// }
+// app.use("/*", (req, res) => {
+//     res.send(path.join(__dirname, "..", "..", "client", "build", "index.html"));
+// });
+
+db.sequelize
+    .sync()
+    .then(console.log("db connected successfully"))
+    .catch((err) => {
+        console.log(error);
+    });
 
 app.listen(PORT);
