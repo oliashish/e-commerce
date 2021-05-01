405403f9be7473aa4bd35595b8dabff2cf2c41dd
Ashish oli <oliashish3@gmail.com>
2021-05-01 15:30:08 +0530

    modified

--- server/src/server.js
+++ server/src/server.js
@@ -11,13 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, "../../client/build")));
 app.use("/", route);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(__dirname, "../../client/build"));
+    app.use(express.static("../../client/build"));
 }
-app.get("/", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
