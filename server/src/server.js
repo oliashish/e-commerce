06c864a1ca2ce33879efcb24df67f7c678a5aad6
Ashish oli <oliashish3@gmail.com>
2021-05-01 15:17:15 +0530

    changed for errors

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/", route);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../../client/build"));
+    app.use(express.static(__dirname, "../../client/build"));
 }
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../../client/build/index.html"));
