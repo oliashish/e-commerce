8855db9dda6fd0b327a60b3dc6da6796f9c11e8c
Ashish oli <oliashish3@gmail.com>
2021-05-01 12:55:23 +0530

    modified server.js file, scripts and client repo

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import Axios from "axios";
-
-Axios.defaults.baseURL = "http://localhost:5000";
 
 ReactDOM.render(<App />, document.getElementById("root"));
