6d06eb12cc5a59e6216d246d732f8f38c752af94
Ashish oli <oliashish3@gmail.com>
2021-05-09 13:48:24 +0530

    setup tailwindcss in client repo

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import "./src/styles/main.css"
 import { BrowserRouter } from "react-router-dom";
-
-
 
 ReactDOM.render(
     <BrowserRouter>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,5 @@
 import React from "react";
 
-import "../styles/Home.css";
-
 const Home = () => {
     return (
         <div className="home-page">
