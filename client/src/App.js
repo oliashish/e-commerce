01c71e4fcbc0fcb4b2e6553d3d7163af8b33d60d
Ashish oli <oliashish3@gmail.com>
2021-05-19 19:45:11 +0530

    created: carousal animation

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
     return (
         <div className="App">
             <Switch>
-                <Route exact path="/" component={Home} />
+                <Route exact path="/" component={Home} />   
             </Switch>
         </div>
     );
