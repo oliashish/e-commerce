752cc615346c6934452916ee33541b763cda2a72
Ashish oli <oliashish3@gmail.com>
2021-06-09 01:50:29 +0530

    authentication branch refracted

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import axios from "axios";
 import { Switch, Route } from "react-router-dom";
 
 import "../src/styles/main.css";
 import Home from "./pages/Home";
 import Auth from "./pages/Auth";
-import { connect } from "react-redux";
+
+import Header from "./components/mainHeader/Header";
 
 axios.defaults.baseURL = "http://localhost:5000";
 axios.defaults.withCredentials = true;
 
-const mapStateToProps = (state) => {
-    return {
-        user: state.auth,
-    };
-};
-
 const App = (props) => {
-    const [user, setUser] = useState(props);
-
     return (
         <div className="App">
+            <Header />
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/authenticate" component={Auth} />
@@ -29,4 +23,4 @@ const App = (props) => {
     );
 };
 
-export default connect(mapStateToProps)(App);
+export default App;
