0776766cdc82be02b5d9ba2dece904166f8085ff
oliashish <oliashish3@gmail.com>
2021-07-12 22:21:36 +0530

    cleanup UI

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,7 @@
 import React from "react";
 
-import CoverCarousal from "../components/Hero/coverCarousal";
 import Footer from "../components/Footer";
 
-import CategoryCard from "../components/ProductListingCards/CategoryCard";
-import ProdByBrands from "../components/ProductListingCards/ProdByBrands";
 import ProductItems from "../components/ProductListingCards/ProductItems";
 import Header from "../components/MainHeader/Header";
 
@@ -12,10 +9,7 @@ const Home = () => {
     return (
         <div className="home-page">
             <Header />
-            <CoverCarousal />
-            <CategoryCard />
             <ProductItems />
-            <ProdByBrands />
             <Footer />
         </div>
     );
