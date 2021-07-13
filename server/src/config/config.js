377d21dcee86b63155e9f1a6efabf3f83c7553a2
oliashish <oliashish3@gmail.com>
2021-07-14 04:38:33 +0530

    database connection server

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -4,17 +4,17 @@ console.log(process.env.DB_USERNAME);
 
 module.exports = config = {
     development: {
+        username: "root",
+        password: "mysql001",
+        database: "sql6425008",
+        host: "localhost",
+        dialect: "mysql",
+    },
+    production: {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DATABASE,
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT,
-    },
-    production: {
-        username: "root",
-        password: null,
-        database: "database_production",
-        host: "127.0.0.1",
-        dialect: "mysql",
     },
 };
