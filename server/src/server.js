40f7959d86a855a2ffec95d76c23ff608a891d7e
oliashish <oliashish3@gmail.com>
2021-07-29 23:51:46 +0530

    fix: image dest issues

--- server/src/server.js
+++ server/src/server.js
@@ -40,7 +40,7 @@ app.use(
 app.use(compression());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, ".." + ".." + "client" + "build")));
+app.use(express.static(path.join(__dirname, "../../client/build/")));
 
 // routing
 
@@ -52,12 +52,10 @@ app.use("/api/user/", user);
 // rendering react frontend from client directory
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../../client/build"));
+    app.use(express.static("../../client/build/"));
 }
 app.get("*", (req, res) => {
-    res.sendFile(
-        path.join(__dirname, ".." + ".." + "client" + "build" + "index.html")
-    );
+    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
 app.listen(PORT, () => {
