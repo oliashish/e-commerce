aed319b8356dc5196ecc9b60331f75cde6cd7760
oliashish <oliashish3@gmail.com>
2021-07-29 23:43:34 +0530

    multer destination fix

--- server/src/server.js
+++ server/src/server.js
@@ -40,7 +40,7 @@ app.use(
 app.use(compression());
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../../client/build")));
+app.use(express.static(path.join(__dirname, ".." + ".." + "client" + "build")));
 
 // routing
 
@@ -55,7 +55,9 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("../../client/build"));
 }
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+    res.sendFile(
+        path.join(__dirname, ".." + ".." + "client" + "build" + "index.html")
+    );
 });
 
 app.listen(PORT, () => {
