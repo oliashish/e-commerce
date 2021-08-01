8e3776827a959ca59f61ae376471e049d92bc01b
oliashish <oliashish3@gmail.com>
2021-08-01 09:05:12 +0530

    performace : code splitting via lazy load and suspence

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import axios from "axios";
 import { Switch, Route } from "react-router-dom";
 
 import "../src/styles/main.css";
+
+import Header from "./components/MainHeader/Header";
 import Home from "./pages/Home";
 
-import IndividualProductDetails from "./components/ProductDetails/IndividualProductDetails";
-import { Cart } from "./pages/Cart/Cart";
-import Header from "./components/MainHeader/Header";
-import Login from "./components/form/Login";
-import Signup from "./components/form/Signup";
-import UserAddress from "./components/shipping/UserAddress";
-import Payment from "./components/shipping/payment/Payment";
-import PlaceOrder from "./components/shipping/PlaceOrder";
-import Category from "./components/ProductDetails/Category";
+const IndividualProductDetails = lazy(() =>
+    import("./components/ProductDetails/IndividualProductDetails")
+);
+const { Cart } = lazy(() => import("./pages/Cart/Cart"));
+const Login = lazy(() => import("./components/form/Login"));
+const Signup = lazy(() => import("./components/form/Signup"));
+const UserAddress = lazy(() => import("./components/shipping/UserAddress"));
+const Payment = lazy(() => import("./components/shipping/payment/Payment"));
+const PlaceOrder = lazy(() => import("./components/shipping/PlaceOrder"));
+const Category = lazy(() => import("./components/ProductDetails/Category"));
 
 axios.defaults.baseURL = "https://aura-apex.herokuapp.com";
-// axios.defaults.baseURL = "http://localhost:5000";
+// axios.defaults.baseURL = "http://localhost:5000";    
 
 axios.defaults.withCredentials = true;
 
@@ -26,28 +29,37 @@ const App = (props) => {
             <Header />
             <Switch>
                 <Route exact path="/" component={Home} />
-                <Route exact path="/login" component={Login} />
-                <Route exact path="/signup" component={Signup} />
+                <Suspense fallback="">
+                    <Route exact path="/login" component={Login} />
 
-                <Route
-                    exact
-                    path="/product/:productId"
-                    component={IndividualProductDetails}
-                />
-                <Route exact path="/category/:cat" component={Category} />
+                    <Route exact path="/signup" component={Signup} />
 
-                <Route exact path="/cart" component={Cart} />
-                <Route exact path="/cart/:productId?" component={Cart} />
-                <Route exact path="/shipping/address" component={UserAddress} />
-                <Route exact path="/shipping/Payment" component={Payment} />
+                    <Route
+                        exact
+                        path="/product/:productId"
+                        component={IndividualProductDetails}
+                    />
 
-                <Route
-                    exact
-                    path="/shipping/placeorder"
-                    component={PlaceOrder}
-                />
+                    <Route exact path="/:cat" component={Category} />
 
-                {/* <Route exact path="/shipping/payment" component={Payment} /> */}
+                    <Route exact path="/cart" component={Cart} />
+
+                    <Route exact path="/cart/:productId?" component={Cart} />
+
+                    <Route
+                        exact
+                        path="/shipping/address"
+                        component={UserAddress}
+                    />
+
+                    <Route exact path="/shipping/payment" component={Payment} />
+
+                    <Route
+                        exact
+                        path="/shipping/placeorder"
+                        component={PlaceOrder}
+                    />
+                </Suspense>
             </Switch>
         </div>
     );
